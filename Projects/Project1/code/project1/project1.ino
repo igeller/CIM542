8c4a93c1f27478d91e0002b79db08d0e3be154cf
IG <ieg23@miami.edu>
2020-02-09 17:07:41 -0500

    s

--- Projects/Project1/code/project1/project1.ino
+++ Projects/Project1/code/project1/project1.ino
@@ -1,4 +1,5 @@
-int ledPin = 3;    // LED connected to digital pin 9
+int ledPin1 = 3;    // LED connected to digital pin 3 and 9
+int ledPin2 = 9;
 int fadeDel = 1;
 int lightDarkDel = 2000;
 double fadeChng = 0.075;
@@ -6,28 +7,40 @@ double fadeValue = 0;
 
 //need to fix the rapid start of the light coming back on
 void setup() {
-    pinMode(ledPin, OUTPUT); //set pin 2 as OUTPUT
-    digitalWrite(ledPin, LOW);
+    pinMode(ledPin1, OUTPUT); 
+    digitalWrite(ledPin1, LOW);
+
+    pinMode(ledPin2, OUTPUT); //set pin 2 as OUTPUT
+    digitalWrite(ledPin2, LOW);
 }
+
+//short leg is flat side, and long side goes in row with cathode
 
 void loop() {
   // fade in from min to max in increments of 5 points:
   
   for (fadeValue; fadeValue <= 255; fadeValue += fadeChng) {
+        // wait for fadeDel milliseconds to see the dimming effect
         delay(fadeDel);
-    // sets the value (range from 0 to 255):
-    analogWrite(ledPin, fadeValue);
-    // wait for fadeDel milliseconds to see the dimming effect
+        // sets the value (range from 0 to 255):
+        analogWrite(ledPin1, fadeValue);
+
+        // sets the value (range from 0 to 255):
+        analogWrite(ledPin2, fadeValue);
   }
 
  //delay(lightDarkDel);
 
   // fade out from max to min in increments of 0.1 points:
   for (fadeValue; fadeValue >= 0; fadeValue -= fadeChng) {
-    // sets the value (range from 0 to 255):
-    analogWrite(ledPin, fadeValue);
-    // wait for fadeDel milliseconds to see the dimming effect
-    delay(fadeDel);
+        // sets the value (range from 0 to 255):
+        analogWrite(ledPin1, fadeValue);
+
+        // sets the value (range from 0 to 255):
+        analogWrite(ledPin2, fadeValue);
+        
+        // wait for fadeDel milliseconds to see the dimming effect
+        delay(fadeDel);
   }
   
   delay(lightDarkDel);
--- Projects/Project1/readme.md
+++ Projects/Project1/readme.md
@@ -1,1 +1,4 @@
 # Project 1: Light and Emotion
+
+## Project Info: 
+The goal of the purpose of the project was to combine our knowledge of led programming and the cricuit cutter.
