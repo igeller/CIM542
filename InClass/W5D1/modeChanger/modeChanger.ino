257f7e7f3ffe9dcde8ef5404d2aaf7461cce146a
IG <ieg23@miami.edu>
2020-02-11 14:09:38 -0500

    added W5D1 info

---
+++ InClass/W5D1/modeChanger/modeChanger.ino
@@ -0,0 +1,57 @@
+int ledPin = 3;
+int switchPin = 2;
+int switchRead = 0;
+int mode = 0;
+
+boolean pressing = false;
+
+//Mode 2 Variables
+int counter = 0;
+
+long pMillis = 0;
+
+int interval = 100;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
+  pinMode(switchPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  switchRead = digitalRead(switchPin);
+  //Serial.println(switchRead);
+
+  if (switchRead == 1) {
+    pressing = true;
+  }
+
+  if (switchRead == 0 && pressing == true) {
+    pressing = false;
+    //do something
+    //counter = counter + 1;
+    mode++;
+    Serial.println(mode);
+  }
+
+  if (mode == 0) {
+    //Reset of Off Condition
+    digitalWrite(ledPin, LOW);
+    Serial.println("Mode 0");
+
+  }
+  else if (mode == 1) {
+    digitalWrite(ledPin, HIGH);
+    Serial.println("Mode 1");
+
+  } else if (mode == 2) {
+    //run Mode program
+    Serial.println("Mode 2");
+
+  } else {
+    mode = 0;
+  }
+
+}
