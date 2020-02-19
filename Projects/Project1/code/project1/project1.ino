eff8ac823f21d81a42ea4bddb9ac9c60037102e8
IG <ieg23@miami.edu>
2020-02-19 12:35:19 -0500

    final code for project added

--- Projects/Project1/code/project1/project1.ino
+++ Projects/Project1/code/project1/project1.ino
@@ -1,6 +1,6 @@
 int ledPin1 = 3;    // LED connected to digital pin 3 and 9
 int ledPin2 = 6;
-int ledPin3 = 9; 
+int ledPin3 = 9;
 int pushButton = 2;
 
 boolean buttonPushed = false;
@@ -10,11 +10,9 @@ long pMillis = 0;
 int interval = 10;
 boolean flipFlop = false;
 double counter = 0;
-
-int lightDarkDel = 2000;
 double fadeChngDwn = 1;
 double fadeChngUp = 0.5;
-double fadeValue = 0;
+
 
 //short leg is flat side, and long side goes in row with resistor
 
@@ -22,14 +20,10 @@ double fadeValue = 0;
 void setup() {
 
   Serial.begin(9600);
-    pinMode(pushButton, INPUT);
-    pinMode(ledPin1, OUTPUT); 
-//    digitalWrite(ledPin1, LOW);
-
-    pinMode(ledPin2, OUTPUT); //set pin 2 as OUTPUT
-//    digitalWrite(ledPin2, LOW);
-
-    pinMode(ledPin3, OUTPUT); //set pin 2 as OUTPUT
+  pinMode(pushButton, INPUT);
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT); //set pin 2 as OUTPUT
+  pinMode(ledPin3, OUTPUT); //set pin 3 as OUTPUT
 }
 
 
@@ -37,30 +31,30 @@ void loop() {
 
   //button stuff so we need a counter
   int buttonState = digitalRead(pushButton);
-  if(buttonState == 1){ // light on
+  if (buttonState == 1) { // light on
     buttonPushed = true;
   }
 
-  if(buttonState == 0 && buttonPushed == true){ //light off
+  if (buttonState == 0 && buttonPushed == true) { //light off
     buttonPushed = false;
     mode++;
   }
 
-  if(mode == 0){ //light off
-      digitalWrite(ledPin1, LOW);
-      digitalWrite(ledPin2, LOW);
-      digitalWrite(ledPin3, LOW);
-  }else if(mode == 1){ // light on
-      digitalWrite(ledPin1, HIGH);
-      digitalWrite(ledPin2, HIGH);
-      digitalWrite(ledPin3, HIGH);
-      counter = 0;
-  }else if(mode == 2){ //pattern on
-      Serial.println(mode);
-      millisPat();
-  }else{
-       mode = 0;  
-  }   
+  if (mode == 0) { //light off
+    digitalWrite(ledPin1, LOW);
+    digitalWrite(ledPin2, LOW);
+    digitalWrite(ledPin3, LOW);
+  } else if (mode == 1) { // light on
+    digitalWrite(ledPin1, HIGH);
+    digitalWrite(ledPin2, HIGH);
+    digitalWrite(ledPin3, HIGH);
+    counter = 0;
+  } else if (mode == 2) { //pattern on
+    Serial.println(mode);
+    millisPat();
+  } else {
+    mode = 0;
+  }
 }
 
 void millisPat() {
@@ -76,7 +70,7 @@ void millisPat() {
 
 
   analogWrite(ledPin1, counter);
-    analogWrite(ledPin2, counter);
+  analogWrite(ledPin2, counter);
   analogWrite(ledPin3, counter);
 
 
@@ -84,7 +78,7 @@ void millisPat() {
     flipFlop = true;
   }
 
-  if(counter <= 0){
+  if (counter <= 0) {
     flipFlop = false;
     delay(2000);
   }
